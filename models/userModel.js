1079db8ef4f115abd685550729a0b02c24412b63
MKSky <ruijieliu814@gmail.com>
2025-03-01 18:01:38 +0800

    update pool

--- models/userModel.js
+++ models/userModel.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
     user: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "root1234",
     database: process.env.DB_NAME || "myapp",
